59ecff6bd08a8d15acc01c30a592a67a4cd833ae
kristjanparna <parna.kristjan@gmail.com>
2022-12-02 16:46:22 +0200

    Added watchlist table filtering

--- src/views/WatchListView.vue
+++ src/views/WatchListView.vue
@@ -1,45 +1,43 @@
 <template>
 
-  <div class="container">
+  <div class="container watchlistBackground">
     <Navbar/>
     <div class="row">
-      <div class="col-6">
-        <table class="table table-hover">
-          <thead>
+      <div class="col">
+        <table class="table table-hover watchlistTable">
+          <thead class="">
           <tr>
-            <th scope="col">Instrument</th>
-            <th scope="col">Hetkehind</th>
-            <th scope="col">Muutus täna</th>
-            <th scope="col">Hind lisamisel</th>
-            <th scope="col">Muutus lisamisest</th>
-            <th scope="col">Ülemine piirhind</th>
-            <th scope="col">Alumine piirhind</th>
-            <th scope="col">Kommentaar</th>
-            <th scope="col">Lisamise kuupäev</th>
-            <th scope="col">Lisainfo</th>
+            <th v-on:mouseover="" v-on:click="sortBy('shortName')">Instrument  <font-awesome-icon class="iconStyleEditBlack" icon="fa-solid fa-sort" /> </th>
+            <th v-on:click="sortBy('currentPrice')">Hetkehind  <font-awesome-icon class="iconStyleEditBlack" icon="fa-solid fa-sort" /> </th>
+            <th v-on:click="sortBy('priceChangePercentage')">Muutus täna  <font-awesome-icon class="iconStyleEditBlack" icon="fa-solid fa-sort" /> </th>
+            <th v-on:click="sortBy('priceAtAddition')">Hind lisamisel  <font-awesome-icon class="iconStyleEditBlack" icon="fa-solid fa-sort" /> </th>
+            <th>Muutus lisamisest</th>
+            <th v-on:click="sortBy('priceHigher')">Ülemine piirhind  <font-awesome-icon class="iconStyleEditBlack" icon="fa-solid fa-sort" /> </th>
+            <th v-on:click="sortBy('priceLower')">Alumine piirhind  <font-awesome-icon class="iconStyleEditBlack" icon="fa-solid fa-sort" /> </th>
+            <th v-on:click="sortBy('userComment')">Kommentaar  <font-awesome-icon class="iconStyleEditBlack" icon="fa-solid fa-sort" /> </th>
+            <th v-on:click="sortBy('additionDate')">Lisamise kuupäev  <font-awesome-icon class="iconStyleEditBlack" icon="fa-solid fa-sort" /> </th>
+            <th>Lisainfo</th>
           </tr>
           </thead>
-          <tbody class="searchTable">
-          <tr>
-            <td> {{ response.shortname }}</td>
+          <tbody>
+          <tr v-for="response in sortedResponses">
+            <td> {{ response.shortName }}</td>
             <td> {{ response.currentPrice }} {{ response.currency }}</td>
-            <td> {{ response.priceChangePercentage }}</td>
-            <td> {{ response.addedPrice }}</td>
-            <td> {{ -(response.addedPrice - response.currentPrice) }} {{ response.currency }}</td>
-            <td> {{ response.highAlertPrice }} {{ response.currency }}</td>
-            <td> {{ response.lowAlertPrice }} {{ response.currency }}</td>
-            <td> {{ response.comment }}</td>
-            <td> {{ response.addedDate }}</td>
-            <td>{{ }}</td>
-            <td class="submitButton">
-                  <button v-on:click="navigateToInstrumentView" class="btn btn-dark" type="button">Lisainfo</button>
-            </td>
+            <td> {{ response.priceChangePercentage }} %</td>
+            <td> {{ response.priceAtAddition }}</td>
+            <td> {{ -(response.priceAtAddition - response.currentPrice) }} {{ response.currency }}</td>
+            <td> {{ response.priceHigher }} {{ response.currency }}</td>
+            <td> {{ response.priceLower }} {{ response.currency }}</td>
+            <td> {{ response.userComment }}</td>
+            <td> {{ response.additionDate }}</td>
+            <td><button class="btn-outline-dark btn justify-content-end" type="button">Lisainfo</button></td>
+
+
           </tr>
           </tbody>
         </table>
       </div>
-
-
+    </div>
   </div>
 </template>
 
@@ -52,33 +50,43 @@ export default {
   data: function () {
     return {
       userId: '',
-      response: {
-        symbol: 'EGR1',
-        shortname: 'Enefit',
-        currentPrice: 25.78,
-        priceChangePercentage: 0.0,
-        currency: 'EUR',
-        addedPrice: 17.55,
-        highAlertPrice: 0,
-        lowAlertPrice: 0,
-        comment: 'kommentaar',
-        addedDate: 'eile'
-      }
+      currentSort: '',
+      currentSortDirection: 'asc',
+      responses: [
+        {
+          ticker: '',
+          shortName: '',
+          currency: '',
+          currentPrice: 0,
+          priceChangePercentage: 0,
+          priceHigher: 0,
+          priceLower: 0,
+          userComment: '',
+          priceAtAddition: 0,
+          additionDate: ''
+        }
+      ]
     }
   },
   methods: {
+    sortBy:function(s) {
+      this.currentSort = s;
+      if (s === this.currentSort) {
+        this.currentSortDirection = this.currentSortDirection === 'asc' ? 'desc' : 'asc';
+      }
+      // this.currentSort = s;
+    },
     getUserId: function () {
       this.userId = sessionStorage.getItem('userId')
     },
     getWatchlistData: function () {
       this.$http.get("/watchlist", {
             params: {
-              watchlistRequest: this.response,
               userId: this.userId
             }
           }
       ).then(response => {
-        response.data = this.response
+        this.responses = response.data
       }).catch(error => {
         console.log(error)
       })
@@ -87,11 +95,26 @@ export default {
       // this.$router.push(name: 'InstrumentInfoRoute')
     },
   },
-  mounted() {
-    this.getUserId()
+  computed:{
+    sortedResponses:function () {
+      return this.responses.sort((a, b) => {
+        let modifier = 1;
+        if (this.currentSortDirection === 'desc') {
+          modifier = -1;
+        }
+        if (a[this.currentSort] < b[this.currentSort]) {
+          return -1 * modifier;
+        }
+        if (a[this.currentSort] > b[this.currentSort]) {
+          return 1 * modifier;
+        }
+        return 0;
+      })
+    }
   },
   beforeMount() {
-    this.getWatchlistData(this.response.symbol, this.userId)
+    this.getUserId()
+    this.getWatchlistData()
   }
 }
 </script>
