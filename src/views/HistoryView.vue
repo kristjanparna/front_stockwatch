eee4a92c8722b6c9923b5771c91fd8d2ea9ae728
Andres Orgla <aorgla@gmail.com>
2022-12-07 09:11:46 +0200

    Added search, edit and remove functionality for admin panel

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -22,9 +22,25 @@ export default {
     getUserId: function () {
       this.userId = sessionStorage.getItem('userId')
     },
+    getTransactionHistory: function () {
+      this.$http.get("/history", {
+            params: {
+              userId: sessionStorage.getItem('userId')
+
+            }
+          }
+      ).then(response => {
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
   },
   mounted() {
     this.getUserId()
+  },
+  beforeMount() {
+    this.getTransactionHistory()
   }
 }
 </script>
